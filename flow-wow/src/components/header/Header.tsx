6706327e529ffdf98d00ad66ac31272a6f5ca678
Konstantin <k.moskal9901@gmail.com>
2023-05-24 15:26:16 +0300

    new star svg

--- flow-wow/src/components/header/Header.tsx
+++ flow-wow/src/components/header/Header.tsx
@@ -6,7 +6,7 @@ function Header(){
     return (
         <header className="header-wraper">
             <div className="header-logo">
-                <div className='star-logo'><img src="/img/star.png" alt="" /></div>
+                <div className='star-logo'><img src="/img/star.svg" alt="" /></div>
                 <a href="https://flowwow.com/"><img src="/img/Vector.png" alt="LOGO" /></a>
             </div>
         </header>
