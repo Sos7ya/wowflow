587b5510577fdf0fcf1539d863c6ba13e2aabf13
Konstantin <k.moskal9901@gmail.com>
2023-05-29 12:12:45 +0300

    new rules

--- flow-wow/src/components/game/GamePlate.jsx
+++ flow-wow/src/components/game/GamePlate.jsx
@@ -21,10 +21,10 @@ export default function GamePlate(){
         const papa =  document.getElementById('container')
         function R(min,max) {return min+Math.random()*(max-min)};
         const w = 10;
-        const total = 25;
+        const total = 8;
         for(let i=1; i<total; i++){
             const test = document.createElement('div');
-            TweenLite.set(test,{attr:{class:'element'},x:R(-10,w),y:-500});
+            TweenLite.set(test,{attr:{class:'element'},x:R(-10,w),y:-100});
             anim(test);
             test.style.cursor = 'pointer';
             test.addEventListener('click',()=>{
@@ -37,7 +37,7 @@ export default function GamePlate(){
         }
 
         function anim(elm){
-            TweenMax.to(elm,R(10,25),{y:850,ease:Linear.easeNone,repeat:-1,delay:-30});
+            TweenMax.to(elm,R(10,25),{y:650,ease:Linear.easeNone,repeat:-1,delay:-30});
             TweenMax.to(elm,R(2,8),{rotation:(360),repeat:-1,yoyo:false,ease:Sine.easeInOut,delay:-5});
             // TweenMax.to(elm, {scale:R(3, 6)});
         }
